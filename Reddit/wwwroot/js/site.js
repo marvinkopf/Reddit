645a9f93c919237078f4f0a859d5b7cd2623c21c
Marvin Kopf <marvinkopf@posteo.de>
2017-04-21 02:12:31 +0200

    add js function to post form
    Allows for a form to post against a rest api and have the form itself decide which resource to load afterwards

--- Reddit/wwwroot/js/site.js
+++ Reddit/wwwroot/js/site.js
@@ -1,1 +1,11 @@
 // Write your Javascript code.
+
+function post_form(form, target) {
+    var fields = {};
+    
+    $(form).find("input").each(function() {
+                fields[$(this).attr("name")] = $(this).val();
+        });
+
+    $.post(target, fields);
+}
