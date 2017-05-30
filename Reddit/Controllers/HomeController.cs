6af004301993a498402555960bd68dc73027b24a
Marvin Kopf <marvinkopf@posteo.de>
2017-05-30 22:06:11 +0200

    remove dead actions from home controller

--- Reddit/Controllers/HomeController.cs
+++ Reddit/Controllers/HomeController.cs
@@ -111,20 +111,6 @@ namespace Reddit.Controllers
                             .First(u => u.UserName.ToUpper() == userName.ToUpper()));
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
