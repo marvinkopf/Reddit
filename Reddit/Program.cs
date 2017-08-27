824ab51efc9022328019d4d30230b04c5072522e
Marvin Kopf <marvinkopf@posteo.de>
2017-08-27 19:00:07 +0200

    create dockerfile

--- Reddit/Program.cs
+++ Reddit/Program.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Reddit
 {
@@ -7,7 +12,11 @@ namespace Reddit
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
---
+++ Reddit/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sqlite3 bin/Debug/netcoreapp1.0/reddit.db < init.sql
